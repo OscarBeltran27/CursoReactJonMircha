f13997da2f76fef06e540c081a4de1a842d2a932
Oscar Beltran <oscarbeltrangaspar@gmail.com>
2021-07-21 14:21:52 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
+import Componente from './components/Componente';
 import './App.css';
+import Propiedades from './components/Propiedades';
+import Estado from './components/Estado';
+import RenderizadoCondicional from './components/RenderizadoCondicional';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg='Hola soy un componente22'/>
+          <hr/>
+          <Propiedades 
+            cadena='Esto es una cadena de texto' 
+            numero={19}
+            booleano={true}
+            arreglo = {[1,2,3]}
+            objeto = {{nombre:'Oscar', correo:'oscar@gmail.com'}}
+            funcion = {(num) => num * num}
+            elementoReact = {<i>Esto es un elemento React</i>}
+            componenteReact = {<Componente msg='Soy un componente pasado'/>}
+          />
+          <hr/>
+          <Estado/>
+          <hr/>
+          <RenderizadoCondicional/>
+        </section>
       </header>
     </div>
   );
